ff3dd33822471549d909e8e7303863f6247d49ea
Ayush Nigam <ayushnigam@Ayushs-MacBook-Air.local>
2022-01-06 16:47:45 +0530

    Updates

---
+++ precache-manifest.83e09805fd22b5d8896254174163b43a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53469e96e0f43c5860c0",
+    "url": "/kafqa-counterapp/static/css/main.f5a448de.chunk.css"
+  },
+  {
+    "revision": "53469e96e0f43c5860c0",
+    "url": "/kafqa-counterapp/static/js/main.53469e96.chunk.js"
+  },
+  {
+    "revision": "1b1128aa1d1f67d9b263",
+    "url": "/kafqa-counterapp/static/js/1.1b1128aa.chunk.js"
+  },
+  {
+    "revision": "135d44e0e382066fa145",
+    "url": "/kafqa-counterapp/static/js/runtime~main.135d44e0.js"
+  },
+  {
+    "revision": "22e1c0935d8ce563f215ac0acb92fa15",
+    "url": "/kafqa-counterapp/index.html"
+  }
+];
